dac92f99f64dce5ccc96441bf72bba94f0c98fbd
Totaram Ramrattan <tramrattan@live.com>
2020-02-02 09:14:11 -0500

    cleanup

--- backend/src/main/java/com/me/projects/articles/controller/WebController.java
+++ backend/src/main/java/com/me/projects/articles/controller/WebController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-
     @GetMapping("/article/{id}")
     public String redirectToArticlePage() {
         return "forward:/index.html";
